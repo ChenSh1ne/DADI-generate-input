d6cab8f2788b6d74082e4891fbf977774820d680
Pasha <pdobrynin@gmail.com>
2015-08-28 18:35:02 +0300

    Style

--- generate-DaDi-input.py
+++ generate-DaDi-input.py
@@ -49,43 +49,38 @@ args = parser.parse_args()
 ref_scaffolds = Fasta(args.ref)
 in_vcf = vcf.Reader(open(args.in_vcf, 'r'))
 
-#print args.pop_one, args.pop_two
-
 def count_alleles(pop, vcf_record):
-    pop_REF_num = 0
-    pop_ALT_num = 0
+    ref = 0
+    alt = 0
     for sample_name in pop:
     	(a1, a2) = vcf_record.genotype(sample_name)['GT'].split('/')
     	if a1 == '1':
-    	    pop_ALT_num += 1
+    	    alt += 1
         else:
-            pop_REF_num += 1
+            ref += 1
         if a2 == '0':
-    	    pop_ALT_num += 1
+    	    alt += 1
         else:
-            pop_REF_num += 1
-    return (pop_REF_num, pop_ALT_num) 
+            ref += 1
+    return (ref, alt) 
 
 out_file = open(args.out_file, 'w')
 header = '%s %s Allele1 %s %s Allele2 %s %s Position\n' % \
          (args.ref_name, args.anc_name, args.pop_one_name,\
-         args.pop_two_name, args.pop_one_name, args.pop_two_name)
+          args.pop_two_name, args.pop_one_name,\
+          args.pop_two_name)
 out_file.write(header)
 
 for record in in_vcf:
     if len(record.ALT) > 1 or record.INFO['NS'] != 7:
     	continue
-    start = record.POS - 2
-    stop  = record.POS + 1
-    ref_trinucl = ref_scaffolds[record.CHROM][start:stop]
-    anc_trinucl = str(ref_trinucl[0]) + record.INFO['AA'] + str(ref_trinucl[-1])
-    pop_one_ref = (count_alleles(args.pop_one, record))[0]
-    pop_one_alt = (count_alleles(args.pop_one, record))[1]
-    pop_two_ref = (count_alleles(args.pop_two, record))[0]
-    pop_two_alt = (count_alleles(args.pop_two, record))[1]
-    out_str     = (str(ref_trinucl), anc_trinucl, record.REF, str(pop_one_ref),\
-                   str(pop_two_ref), str(record.ALT[0]), str(pop_one_alt),\
-                   str(pop_two_alt), str(record.POS), '\n')
+    ref_trinucl      = ref_scaffolds[record.CHROM][record.POS - 2:record.POS + 1]
+    anc_trinucl      = str(ref_trinucl[0]) + record.INFO['AA'] + str(ref_trinucl[-1])
+    (p1_ref, p1_alt) = (count_alleles(args.pop_one, record))
+    (p2_ref, p2_alt) = (count_alleles(args.pop_two, record))
+    out_str          = (str(ref_trinucl), anc_trinucl, record.REF, str(p1_ref),\
+                        str(p2_ref), str(record.ALT[0]), str(p1_alt),\
+                        str(p2_alt), str(record.POS), '\n')
     out_file.write(' '.join(out_str))
 
 out_file.close()
